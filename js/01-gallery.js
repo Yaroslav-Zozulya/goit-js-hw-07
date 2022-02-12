cdee868234d7c2c9417e1fd5ea467bcbe4c6d3bd
Yaroslav Zozulya <my.dev.stories@gmail.com>
2022-02-12 21:09:01 +0300

    first task done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+//Функция отрисовку галереи
+function galleryRender(arrayOfImages) {
+    const imagesArr = [];
+    galleryItems.map(({ preview, original, description }) => {
+        imagesArr.push(`<div class="gallery__item">
+                            <a class="gallery__link" href="large-image.jpg">
+                                <img
+                                class="gallery__image"
+                                src="${preview}"
+                                data-source="${original}"
+                                alt="${description}"
+                                />
+                            </a>
+                        </div>`);
+    });
+    galleryRef.insertAdjacentHTML('beforeend', imagesArr.join(''));
+}
+galleryRender(galleryItems);
+
+galleryRef.addEventListener('click', openOriginalImage); //Слушатель клика на родительский div галереи
+function openOriginalImage(event) {
+    event.preventDefault(); // отменяем дефолтное поведение ссылок
+
+    const instance = basicLightbox.create(
+        // открываем модальное окно
+        `<img src="${event.target.dataset.source}" width="800" height="600">`,
+        {
+            onClose: instance =>
+                document.removeEventListener('keydown', onEscClick), // снимаем слушатель клавиатуры при закрытие модалки
+        },
+    );
+    instance.show();
+
+    document.addEventListener('keydown', onEscClick); // добавляем слушатель клавиатуры
+    function onEscClick(event) {
+        // проверяем как кнопка нажата
+        if (event.code === 'Escape') {
+            // если ESC то закрываем модалку
+            instance.close();
+        }
+    }
+}
